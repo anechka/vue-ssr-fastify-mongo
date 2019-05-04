43a49b8e069ba5e8cb02bebee1893094b00be19c
Anna Novikova <anna@novikova.us>
2019-05-04 18:52:50 +0300

    Vue with PostCSS SugarSS https://github.com/postcss/sugarss

---
+++ src/vue/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    #container
+        h1.title Vue app
+</template>
+
+<script>
+    export default {
+        el: "#container",
+        data() {
+            return {}
+        },
+        methods: {
+        },
+        mounted() {
+            console.log("Vue running...");
+        }
+    }
+</script>
+
+<style lang="sss">
+    BODY
+        font-family: "Helvetica Neue", "Helvetica", "Arial", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", sans-serif
+</style>
